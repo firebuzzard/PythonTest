d3be65cc411021a2857b2b2d26c3437f09633dfe
firebuzzard@gmail.com <24"Pyth0ns>
2016-04-09 14:32:45 -0500

    Added a craptastic hello world python script.

---
+++ HelloWorld.py
@@ -0,0 +1,5 @@
+# This is a Hello World app written just to test Git functionality from Visual Studio 2015 Community Edition.
+# It's weak, but I don't care.  Don't judge.
+
+print("Hello world!")
+print("This is a test change to see how things work as code changes.")
